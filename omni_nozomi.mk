a6879c6a6a800ba51a76b6d48124f5ac64a16217
Great-Geek <glendsouza2691995@hotmail.com>
2013-11-21 22:05:31 +0530

    Initial omni_nozomi makefiles.

---
+++ omni_nozomi.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2013 The Open SEMC Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Arbitrary string, comment out for default 'homemade' builds
+ROM_BUILDTYPE := OpenSEMC
+
+# Torch
+PRODUCT_PACKAGES := \
+    OmniTorch
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from omni custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit sony common resources
+$(call inherit-product, device/sony/common/resources-xhdpi.mk)
+
+# Inherit from nozomi device
+$(call inherit-product, device/sony/nozomi/nozomi.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
+PRODUCT_NAME := omni_nozomi
+PRODUCT_DEVICE := nozomi
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia S
